93276b574f567e80ede97843272b8cbb6c8e917d
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-05 14:57:51 -0600

    First Commit
    + Vagrant file to setup an Ubuntu Precise box
    + setup.sh script to install common packages and nvm
    + rebuild.sh script to destroy and rebuild vagrant box

---
+++ rebuild.sh
@@ -0,0 +1,4 @@
+vagrant destroy -f
+vagrant up
+vagrant ssh -c "sudo sh /vagrant/setup.sh"
+
---
+++ setup.sh
@@ -0,0 +1,4 @@
+apt-get update
+apt-get install -y curl vim git
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
