7f6fb42d5b3b6fec2f215b542a61770ba7962a7b
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-05 16:41:03 -0600

    Set Default Node Version

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 sudo apt-get update
 sudo apt-get install -y curl vim git
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+source ~/.profile
+nvm install v0.10.20
+nvm alias default 0.10.20
 
