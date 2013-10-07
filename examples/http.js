6b821b399f1e79f210727a58663a3bb229c0f979
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-06 19:13:15 -0600

    Moves Examples Into Directory
    + Moves http.js and net.js into examples directory
    + Adds entry in .gitignore to ignore other examples

--- examples/http.js
+++ examples/http.js
@@ -2,5 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
       res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('Hello World\n');
-}).listen(8080, '192.168.200.10');
-console.log('Server running at http://192.168.200.10:8080/');
+}).listen(8080);
--- net.js
+++
@@ -1,6 +0,0 @@
-var net = require('net');
-var server = net.createServer(function (socket) {
-      socket.write('Echo server\r\n');
-        socket.pipe(socket);
-});
-server.listen(1337, '192.168.200.10');
