31aa52b47093826af77a119ce9c99584df210a56
Joshua Stoutenburg <jstoutenburg@deseretdigital.com>
2013-10-08 17:47:32 -0600

    Code Formatting

--- examples/net.js
+++ examples/net.js
@@ -1,11 +1,11 @@
 var net = require('net');
 var server = net.createServer(function (socket) {
-      socket.write('Echo server\r\n');
-      socket.on('data', function (data) {
-          if (data == 'exit\n') {
-              socket.close();
-          }
-          socket.write('You said: ' + data);
-      });
+    socket.write('Echo server\r\n');
+    socket.on('data', function (data) {
+        if (data == 'exit\n') {
+            socket.close();
+        }
+        socket.write('You said: ' + data);
+    });
 });
 server.listen(1337);
